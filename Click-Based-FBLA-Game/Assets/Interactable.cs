a1e41ca4f4f9956c2dbc0c1c688ba598027bcd10
Spazdude2 <spaziani96@gmail.com>
2017-11-25 00:32:24 -0500

    Start of click based game

---
+++ Click-Based-FBLA-Game/Assets/Interactable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Interactable : MonoBehaviour
+{
+    public NavMeshAgent PlayerAgent;
+
+    public virtual void MoveToInteraction(NavMeshAgent PlayerAgent)
+    {
+        this.PlayerAgent = PlayerAgent;
+        PlayerAgent.stoppingDistance = 20f;
+        PlayerAgent.destination = this.transform.position;
+
+        Interact();
+    }
+
+    public virtual void Interact()
+    {
+        Debug.Log("Interacting with base class.");
+    }
+}
